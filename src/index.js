0ca3bd1080b3e9dd65c329005807bd9f73740846
9pasha <pavel.b820@gmail.com>
2019-03-22 16:21:28 +0300

    features add some algorithm

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-/*module.exports = function getLoveTrianglesCount(preferences = []) {*/
+module.exports = function getLoveTrianglesCount(preferences = []) {
 
-	var deleted = [];
-    var preferences = [2, 6, 6, 4, 2, 5, 4, 5];
-    var lenPref = preferences.length - 1; /*7*/
-    var tmp = {};
-    var counter = 0;
+	var counter = 0;
+	var lenPref = preferences.length;
 
-    for (var i = lenPref; i >= 0; i--) {
-        if (preferences[i] in tmp) continue;
-        counter++;
-        deleted.push(preferences[i]);
-        tmp[preferences[i]] = 1;
-	} 
-
-	for (var a = 0; a <= counter - 1; a++) {
-		console.log(deleted[a]);
+	for (var i = 0; i < lenPref; i++) {
+		let next = preferences[i] - 1;
+		if (next == i) continue;
+		let nextnext = preferences[next] - 1;
+		if (nextnext == next) continue;
+		let nextnextnext = preferences[nextnext] - 1;
+		if (nextnextnext == nextnext) continue;
+		if (nextnextnext == i) {
+			counter ++;
+		}
 	}
 
-	/*---Let's try to create algoritm from min to max---*/
-	
-	for () {
-		
-	}
-
-	/*---At the end, try to create algoritm find a triagles---*/
-
+	return counter/3;
+}
